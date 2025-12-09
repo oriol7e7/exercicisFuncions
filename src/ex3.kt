462ffce1bd52df31d49572688e2367bee0eed841
oriol7e7 <oriol.plazas.7e7@itb.cat>
2025-12-09 13:58:28 +0100

    ex3 comprovar radi

--- src/ex3.kt
+++ src/ex3.kt
@@ -1,11 +1,12 @@
 fun main(){
+
     calculVolum(comprovarRadi())
 }
 
 fun comprovarRadi():Int {
     var radi: Int?
     do {
-        println("Posa el radi")
+        println("Posa el valor")
         radi = readln().toIntOrNull()
         if (radi == null) {
             println("Posa un enter")
@@ -20,4 +21,32 @@ fun calculVolum(radi: Int): Double {
 
 fun cub(radi:Int): Int {
     return radi*radi*radi
+}
+
+fun mostraMenu():Int {
+    println("""
+        Que vols calcular?
+        1) Volum esfera
+        2) Area Quadrat
+        3) Area Rectangle
+        4) Area triangle
+    """.trimIndent())
+    var opcio: Int?
+    do {
+        opcio = readln().toIntOrNull()
+        if (opcio == null || opcio !in 1..4) {
+            println("Posa un valor correcte")
+        }
+    }while (opcio == null || opcio !in 1..4)
+
+    return opcio
+}
+
+fun gestorMenu(num: Int) {
+    when(num) {
+        1 -> calculVolum(comprovarRadi())
+        2 -> println("""
+            La area del quadrat es ${comprovarRadi()*comprovarRadi()}
+        """.trimIndent())
+    }
 }
